cf795bd85bdb5520930ba09c2c608b1b19bc7286
Temirlan Mirulan <mirulanow@gmail.com>
2025-09-09 15:40:54 +0200

    Initial Commit

---
+++ practice/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  practice
+//
+//  Created by Temirlan Mirulan on 09.09.25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
