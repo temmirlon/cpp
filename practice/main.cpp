0277b58b92f3b24307424775d0f2e5ab8d3380ba
Temirlan Mirulan <mirulanow@gmail.com>
2025-09-09 16:07:37 +0200

    test_commit

--- practice/main.cpp
+++ practice/main.cpp
@@ -1,14 +1,9 @@
-//
-//  main.cpp
-//  practice
-//
-//  Created by Temirlan Mirulan on 09.09.25.
-//
-
 #include <iostream>
+#include <string>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main(){
+    
+    std::cout<< "Test Push" << std::endl;
+    
     return 0;
 }
