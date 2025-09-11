a14ce1730db286df08f97e95a05e42ceb63ce1ec
Temirlan Mirulan <mirulanow@gmail.com>
2025-09-11 17:48:35 +0200

    from_stringexpression_to_result_commit

--- practice/main.cpp
+++ practice/main.cpp
@@ -1,24 +1,46 @@
 #include <iostream>
 #include <string>
-#include <vector>
-#include <map>
 #include <algorithm>
+#include <vector>
 
-std::string reverse(const std::string& text) {
-    if (text.size() == 0)
-        return "";
-    return reverse(text.substr(1)) + text.at(0);
-}
+// Das Mathegenie
 
-bool isPalindrom(std::string& text){
-    std::transform(text.begin(), text.end(), text.begin(), tolower);
-    if (text.size() == 1 || text == reverse(text))
-        return true;
-    return false;
+int parseComputation(const std::string & input) {
+
+    std::string expression = input;                            // 4 + 20+1 +3
+    std::vector<std::string> numbers;
+
+    while (expression.find(" ") != std::string::npos){
+        expression.erase(expression.find(" "), 1);
+    }
+
+    size_t start = 0;
+    size_t end = expression.find("+");
+
+    while (end != std::string::npos)
+    {
+        numbers.push_back(expression.substr(start, end - start));    // 4+20+1+3
+                                                                    // 01234567
+        start = end + 1;   // start = 2 because end = 1 and + 1 equals to 2
+        end = expression.find("+", start); // .find(we are searching for "+", from start) so 'end' is next position of "+", if "+" not exist anymor
+                                            // then we gonna exit while - loop
+    }
+    numbers.push_back(expression.substr(start, end)); // if didn't find that we looked for
+                                                        // numbers is now = {"4", "2", "1"}
+                                                        // we need to now transform strnumbers into real numbers (int) to get sum of this numbers
+    int result = 0;
+    for (int i = 0; i < numbers.size(); i++)
+        result += std::stoi(numbers.at(i));
+
+    return result;
 }
 
 int main() {
-    std::string word = "Lagerregal";
-    std::cout << isPalindrom(word) << std::endl;
+    
+    std::string word = "4 + 20+1 +3+2+10+4";
+    std::cout << parseComputation(word);
+    
+    //std::cout << word.size() << std::endl; // - word.size() - 5
+    
     return 0;
 }
