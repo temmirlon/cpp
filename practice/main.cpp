ac98c83d529cf2d33775df10b5ca643f79b6487c
Temirlan Mirulan <mirulanow@gmail.com>
2025-09-11 11:17:12 +0200

    cast_commit

--- practice/main.cpp
+++ practice/main.cpp
@@ -2,33 +2,15 @@
 #include <string>
 #include <vector>
 
-//substring
-
-std::vector<std::string> split(const std::string& str, char delimiter = ',') {
-    std::vector<std::string> parts;
-    size_t start = 0;
-    size_t end = str.find(delimiter);
+int main(){
     
-    //Wenn die Suche zu keinem Ergebnis fuhr, nimmt der Ruckgabewert den speziellen Wert npos an.
-
-    while (end != std::string::npos)
-    {
-        parts.push_back(str.substr(start, end - start));
-        start = end + 1;
-        end = str.find(delimiter, start);
-    }
-    parts.push_back(str.substr(start, end));
-    return parts;
-}
-
-int main() {
+    int a = 5, b = 4;
     
-    std::vector<std::string> list = split("Hello, my name is Timo, Have a nice day, bye, bye");
+    double c = a / static_cast<double>(b); // C++ Schreibweise
     
-    for (int i = 0; i < list.size(); i++) {
-        std::cout << list.at(i);
-    }
+    // double c = a / double(b);  // C Schreibweise
     
-    std::cout<<std::endl;
+    std::cout << c << std::endl;
+    
     return 0;
 }
