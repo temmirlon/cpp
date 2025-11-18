60af0d064af559be8bf494575e018edec265fbac
Temirlan Mirulan <mirulanow@gmail.com>
2025-11-18 14:10:02 +0100

    Multithreaded programming #1

--- practice/main.cpp
+++ practice/main.cpp
@@ -1,52 +1,19 @@
 #include <iostream>
 #include <string>
-#include <functional>
-#include <vector>
+#include <thread>
+#include <chrono> // для работы со временем
 
-// Полиморфная оберка функции
-// std::function
-
-
-void foo(int a){                                    // сигнатура у всех должна быть одинаковая
-    
-    if (a > 10 && a < 40) {
-        std::cout << "foo: " << a << std::endl;
-    }
-}
-
-void bar(int a){
-    
-    if (a % 2 == 0) {
-        std::cout << "bar: " << a << std::endl;
-    }
-}
-
-void doWork(std::vector<int> &vc,  std::vector<std::function<void(int)>> funcVector){
-    
-    for (auto element : vc) {
-        for (auto &funcelement : funcVector) {
-            funcelement(element);
-        }
-    }
-}
-
-void baz(int a){
-    std::cout << "baz" << std::endl;
-}
+// Многопоточное программирование
+// Multithreaded programming
 
 int main(){
     
-    std::function<int(int,int)> f;
+    std:: cout << "Start main: " << std::endl;
     
-    std::vector<int> nums = {11,23,53,34,89,9,40,76,37,55,29,23,39,26};
+    std::this_thread::sleep_for(std::chrono::milliseconds(1000)); // задерживает выполнение кода
+    std:: cout << std::this_thread::get_id() << std::endl;
     
-    std::vector<std::function<void(int)>> fVector;
-
-    fVector.emplace_back(foo);
-    fVector.emplace_back(bar);
-    fVector.emplace_back(baz);
-    
-    doWork(nums, fVector);
+    std:: cout << "End main: " << std::endl;
     
     return 0;
 }
