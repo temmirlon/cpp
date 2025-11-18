6c2a4bb98fb017dd7b8e253a5dc5c8330f139f67
Temirlan Mirulan <mirulanow@gmail.com>
2025-11-18 12:25:54 +0100

    std::function #2

--- practice/main.cpp
+++ practice/main.cpp
@@ -1,37 +1,52 @@
 #include <iostream>
 #include <string>
 #include <functional>
-
+#include <vector>
 
 // Полиморфная оберка функции
 // std::function
 
 
-void foo(){
+void foo(int a){                                    // сигнатура у всех должна быть одинаковая
     
-    std::cout << "foo" << std::endl;
-    
+    if (a > 10 && a < 40) {
+        std::cout << "foo: " << a << std::endl;
+    }
 }
 
-void bar(){
+void bar(int a){
     
-    std::cout << "=====bar====="<< std::endl;
-    
+    if (a % 2 == 0) {
+        std::cout << "bar: " << a << std::endl;
+    }
 }
 
-int sum(int a, int b){
-    return a + b;
+void doWork(std::vector<int> &vc,  std::vector<std::function<void(int)>> funcVector){
+    
+    for (auto element : vc) {
+        for (auto &funcelement : funcVector) {
+            funcelement(element);
+        }
+    }
+}
+
+void baz(int a){
+    std::cout << "baz" << std::endl;
 }
 
 int main(){
     
-    
-    
     std::function<int(int,int)> f;
     
-    f = sum;
+    std::vector<int> nums = {11,23,53,34,89,9,40,76,37,55,29,23,39,26};
     
-    std::cout << f(3, 5) << std::endl; 
+    std::vector<std::function<void(int)>> fVector;
+
+    fVector.emplace_back(foo);
+    fVector.emplace_back(bar);
+    fVector.emplace_back(baz);
+    
+    doWork(nums, fVector);
     
     return 0;
 }
