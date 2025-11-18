d2dd74d53957a9f60d8133fbd40ea3a0961143eb
Temirlan Mirulan <mirulanow@gmail.com>
2025-11-18 12:02:06 +0100

    std::function #1

--- practice/main.cpp
+++ practice/main.cpp
@@ -1,38 +1,37 @@
 #include <iostream>
 #include <string>
-#include <map>
-#include <vector>
-// leetcode
+#include <functional>
 
-std::vector<int> topKFrequent(std::vector<int>& nums, int k){
-    std::map<int,int> freq;
 
-    // Считаем количество вхождений каждого числа
-    for (int num : nums) {
-        freq[num]++;
-    }
-    // Копируем пары в вектор для сортировки
-    std::vector<std::pair<int,int>> vec(freq.begin(), freq.end());
+// Полиморфная оберка функции
+// std::function
+
+
+void foo(){
     
-    // Сортируем по значению по убыванию
-    std::sort(vec.begin(), vec.end(),
-                [](const std::pair<int,int>& a, const std::pair<int,int>& b){
-                    return a.second > b.second;
-                });
-    // Берём первые k элементов
+    std::cout << "foo" << std::endl;
     
-    std::vector<int> result;
-    for (int i = 0; i < k && i < vec.size(); i++) {
-        result.push_back(vec[i].first);  // добавляем сам элемент, а не частоту
-    }
+}
+
+void bar(){
     
-    return result;
+    std::cout << "=====bar====="<< std::endl;
+    
+}
+
+int sum(int a, int b){
+    return a + b;
 }
 
 int main(){
     
-    std::vector<int> nums = {1,1,1,3,7,7,1,7,2,2,3};
     
+    
+    std::function<int(int,int)> f;
+    
+    f = sum;
+    
+    std::cout << f(3, 5) << std::endl; 
     
     return 0;
 }
