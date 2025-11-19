4871bfa3b77f6681044061dda0c1d3875ba87ab6
Temirlan Mirulan <mirulanow@gmail.com>
2025-11-19 10:15:08 +0100

    Multithreaded programming #2

--- practice/main.cpp
+++ practice/main.cpp
@@ -6,14 +6,30 @@
 // Многопоточное программирование
 // Multithreaded programming
 
+void doWork(){
+    
+    for (size_t i = 0; i < 10; i++) {
+        
+        std::cout << std::this_thread::get_id() << "\tdoWork\t" << i << std::endl; // поток doWork и main один и тот же
+        std::this_thread::sleep_for(std::chrono::milliseconds(1000)); // задержка
+        
+    }
+    
+}
+
 int main(){
     
-    std:: cout << "Start main: " << std::endl;
+    std::thread th(doWork); // запуск задачи в нашем потоке параллельно (запуск программы в двух потоках)
+    //th.detach(); // Separates the thread of execution from the thread object, allowing execution to continue independently.
     
-    std::this_thread::sleep_for(std::chrono::milliseconds(1000)); // задерживает выполнение кода
-    std:: cout << std::this_thread::get_id() << std::endl;
+    for (size_t i = 0; i < 10; i++) {
+        
+        std::cout << std::this_thread::get_id() << "\tmain\t" << i << std::endl;
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));
+        
+    }
     
-    std:: cout << "End main: " << std::endl;
     
+    th.join(); // Blocks the current thread until the thread identified by *this finishes its execution.
     return 0;
 }
