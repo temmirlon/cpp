0080a469817c99f1f46908586cfc1999d034eba7
Temirlan Mirulan <mirulanow@gmail.com>
2025-11-20 17:38:06 +0100

    sample

--- practice/main.cpp
+++ practice/main.cpp
@@ -1,53 +1,16 @@
 #include <iostream>
 #include <string>
-#include <functional>
 #include <vector>
 
-// Lambda function
-// Лябмда функции
 
-
-void foo(int a){                                    // сигнатура у всех должна быть одинаковая
-    
-    if (a > 10 && a < 40) {
-        std::cout << "foo: " << a << std::endl;
-    }
-}
-
-void bar(int a){
-    
-    if (a % 2 == 0) {
-        std::cout << "bar: " << a << std::endl;
-    }
-}
-
-void baz(int a){
-    std::cout << "baz" << std::endl;
-}
-
-
-
-void doWork(std::vector<int> &vc,  std::function<void(int)> func){
-    
-    for (auto el : vc) {
-        func(el);
-    }
-    
-}
 
 int main(){
     
-    // как лямбда функция определяет какой тип возвращаемого значения у нее есть
+    std::vector<int> nums = {1,4,3,2,5,6,4};
     
-    auto l_one = [](int a, int b){
-        
-        return a + b; // должно что то возвращать
-        
+    for (size_t i = 0; i < nums.size(); ++i) {
+        std::cout << nums.at(i) << std::endl;
     };
-    
-    auto l_two = l_one(4, 3); // type of l_two is "int". depends of what we return from l_one lambda
-    
-    std::cout << l_two << std::endl;
     
     return 0;
 }
