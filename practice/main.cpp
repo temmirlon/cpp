0ad29d80591fb8d9190272f3fde9ecaa01ab9529
Temirlan Mirulan <mirulanow@gmail.com>
2025-12-01 12:17:50 +0100

    OOP #2

--- practice/main.cpp
+++ practice/main.cpp
@@ -11,20 +11,29 @@ struct Point{
 // Поля класса недоступны по умолчанию вне класса
 class Point2D {
     int x,y;
+    
+public:
+    void set_coords(int a, int b)
+    { x = a; y = b; } // {this->x = a;, this->y = b} одно и то же
+    void get_coords(int& a, int& b)
+    { a = x; b = y; }
 };
 
 int main(){
-    
-    Point pt;
-    Point* ptr_pt = new Point;
-    
-    pt.x = 10;
+    // ловим ошибку: потому что не связан ни с каким объектом
+    // неявный указатель this не определен
+    Point2D::set_coords(4, 3);
     
     
-    Point2D pt2d;
-    Point2D *ptr_pt2 = new Point2D;
+    Point2D pt1;
+    Point2D *ptr_pt = new Point2D; // создается в куче (heap), нужно освобождать память в конце
     
-    pt2d.x = 10; // ловим ошибку "x" is a 'PRIVATE' member of 'Point2D'
+    // сюда автоматичекски передаются указатель с именем this
+    // this (указатель) ссылается на тот объект через который был вызван данный метод
+    pt1.set_coords(1, 2);
+    
+    
+    delete ptr_pt;
     
     return 0;
 }
