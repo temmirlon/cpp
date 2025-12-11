6d9543b08156fafe0eab9b75408b5412a54b43e0
Temirlan Mirulan <mirulanow@gmail.com>
2025-12-11 16:10:26 +0100

    #7 oop - transform constructor

--- practice/main.cpp
+++ practice/main.cpp
@@ -1,55 +1,31 @@
 #include <iostream>
-#include <math.h>
-#include <string>
-#include <vector>
 
-// Конструкторы
 
-// Имя конструктора должно совпадать с именем типа данных
-// ... никогда не возвращает никаких значений, поэтому возвращаемый тип не прописывается
-// ... может иметь произвольное (любое) число параметров
-// ... всегда вызывается при создании каждого нового объекта
+/*
+    Конструтор преобразования. Деструктор
+ 
+ */
 
-class Point2D {
 
-    const unsigned max_coord{100};
-    int x{0}, y{0};
-
+class Complex{
+    double re;
+    double im;
+    
 public:
-    Point2D() : x(0), y(0) {} // constructor overloading
+    Complex() : re(0.0), im(0.0) {}
     
-    Point2D(int a, int b) : x(a), y(b)
-            {
-            std::cout << "CONSTRUCTOR" << std::endl;
-        } // Тело конструктора
+    Complex(double real) : re(real), im(0.0) {}
     
-    void set_coords(int a, int b) {
-        x = a;
-        y = b;
+    Complex(double real, double imag) : re(real), im(imag) {}
+    
+    void get_data(double& re, double& im){
+        re = this->re;
+        im = this->im;
     }
-    void get_coords(int& a, int& b) {
-        a = x;
-        b = y;
-    }
-    
 };
+
 
 int main(){
     
-    
-    Point2D pt; // first contructor
-
-    Point2D* ptr_pt = new Point2D(1,2); // second constructor
-    
-    int x, y;
-    
-    pt.get_coords(x, y);
-    std::cout << x << " " << y <<std::endl;
-    
-    ptr_pt->get_coords(x, y);
-    std::cout << x << " " << y <<std::endl;
-    
-    delete ptr_pt;
-    
     return 0;
-};
+}
