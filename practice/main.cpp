8e3126111953990f7ea6e500adbd6d1202000caf
Temirlan Mirulan <mirulanow@gmail.com>
2025-12-12 09:49:32 +0100

    #7 oop - Conversion constructor (Преобразования конструктор)

--- practice/main.cpp
+++ practice/main.cpp
@@ -14,6 +14,7 @@ class Complex{
 public:
     Complex() : re(0.0), im(0.0) {}
     
+    // Конструктор преобразования - Conversion constructor
     Complex(double real) : re(real), im(0.0) {}
     
     Complex(double real, double imag) : re(real), im(imag) {}
@@ -22,10 +23,30 @@ public:
         re = this->re;
         im = this->im;
     }
+    
+    const Complex& Add(Complex& other){
+        this->re += other.re;
+        this->im += other.im;
+        
+        return *this;
+    }
+    
+    
 };
 
 
 int main(){
+    
+    Complex c1;
+    Complex c2(0.5); // {0.5} or c2 = 0.5
+    Complex c3{-5.4, 7.8};
+    
+    c2.Add(c3);
+    
+    double re, im;
+    c2.get_data(re, im);
+    
+    std::cout << re << " " << im << std::endl;
     
     return 0;
 }
